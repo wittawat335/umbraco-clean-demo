69ff61d38896804d9c8f1496dcf9ee9763d127ff
Wittawat Fuangfu <wittawat.f@softdebut.com>
2024-12-20 10:42:51 +0700

    #

--- umbraco-clean-demo.Application/MappingProfiles/MappingProfile.cs
+++ umbraco-clean-demo.Application/MappingProfiles/MappingProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using umbraco_clean_demo.Domain.Entities;
+using umbraco_clean_demo.Domain.Entities.Kentico;
 namespace umbraco_clean_demo.Application.MappingProfiles;
 
 public class MappingProfile : Profile
@@ -8,13 +9,20 @@ public class MappingProfile : Profile
 	{
 		// Map Role -> umbracoUserGroup
 		CreateMap<Role, umbracoUserGroup>()
-			//.ForMember(dest => dest.id, opt => opt.MapFrom(src => src.RoleID))
 			.ForMember(dest => dest.userGroupAlias, opt => opt.MapFrom(src => src.RoleName.ToLower().Replace(" ", "_")))
 			.ForMember(dest => dest.userGroupName, opt => opt.MapFrom(src => src.RoleName))
 			.ForMember(dest => dest.createDate, opt => opt.MapFrom(src => src.RoleLastModified))
 			.ForMember(dest => dest.updateDate, opt => opt.MapFrom(src => src.RoleLastModified))
 			.ForMember(dest => dest.hasAccessToAllLanguages, opt => opt.MapFrom(src => true));
-			//.ForMember(dest => dest.startContentId, opt => opt.MapFrom(src => src.RoleGroupID))
-			//.ForMember(dest => dest.startMediaId, opt => opt.MapFrom(src => src.RoleIsDomain));
+
+		CreateMap<Users, umbracoUser>()
+			.ForMember(dest => dest.userDisabled, opt => opt.MapFrom(src => src.UserEnabled))
+			.ForMember(dest => dest.userName, opt => opt.MapFrom(src => src.UserName))
+			.ForMember(dest => dest.userLogin, opt => opt.MapFrom(src => src.UserName))
+			.ForMember(dest => dest.userEmail, opt => opt.MapFrom(src => src.Email))
+			.ForMember(dest => dest.userLanguage, opt => opt.MapFrom(src => src.PreferredUICultureCode))
+			.ForMember(dest => dest.lastLoginDate, opt => opt.MapFrom(src => src.LastLogon))
+			.ForMember(dest => dest.createDate, opt => opt.MapFrom(src => src.UserCreated))
+			.ForMember(dest => dest.updateDate, opt => opt.MapFrom(src => src.UserLastModified));
 	}
 }
--- umbraco-clean-demo.Application/Services/UsersService.cs
+++ umbraco-clean-demo.Application/Services/UsersService.cs
@@ -1,4 +1,5 @@
-﻿using Umbraco.Cms.Core.Services;
+﻿using AutoMapper;
+using Umbraco.Cms.Core.Services;
 using umbraco_clean_demo.Application.Interfaces;
 using umbraco_clean_demo.Domain.Entities;
 using umbraco_clean_demo.Domain.Entities.Kentico;
@@ -7,17 +8,18 @@ using umbraco_clean_demo.Infrastructure.Utilities;
 
 namespace umbraco_clean_demo.Application.Services;
 
-public class UsersService(IMigrateRepository<Users> _repository, IUserService _service) : IUsersService
+public class UsersService(IMigrateRepository<Users> _repository, IUserService _service, IMapper _mapper) : IUsersService
 {
 	public async Task<Response<string>> MigrateUsers(MigrateModel model)
 	{
 		var response = new Response<string>();
 		var users = await _repository.GetAllAsync(Constants.K_Table.Users, model);
-		foreach (var item in users.Take(10).GroupBy(u => u.UserID))
+		var umbracoUser = _mapper.Map<List<umbracoUser>>(users);
+		foreach (var item in umbracoUser)
 		{
-			if (_service.GetByUsername(item.First().UserName) == null)
+			if (_service.GetByUsername(item.userName) == null)
 			{
-				var user = _service.CreateUserWithIdentity(item.First().UserName, item.First().Email);
+				var user = _service.CreateUserWithIdentity(item.userName, item.userEmail);
 				_service.Save(user);
 			}
 		}
